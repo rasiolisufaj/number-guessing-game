eb2dce416487d46b142ae7f4fc0621c601a82fbc
Rasiol Isufaj <rasiolisufaj47@gmail.com>
2022-09-25 17:34:40 +0200

    Demo 1

---
+++ script.js
@@ -0,0 +1,31 @@
+const userInput = document.getElementById("user-input");
+const messageBoxElement = document.getElementById("msg");
+const spanBoxElement = document.getElementById("span-box");
+const popupElement = document.getElementById("popup");
+const guessedNumberElement = document.getElementById("guessed-number");
+const playAgainBtn = document.querySelector(".play-again");
+let generatedNumber = Math.floor(Math.random() * 100 + 1);
+console.log(generatedNumber);
+
+userInput.addEventListener("focusout", () => {
+  messageBoxElement.classList.remove("d-none");
+  if (+userInput.value === generatedNumber) {
+    popupElement.classList.remove("d-none");
+    guessedNumberElement.innerText = generatedNumber;
+  } else if (userInput.value < generatedNumber) {
+    spanBoxElement.innerText = userInput.value;
+    messageBoxElement.lastElementChild.innerHTML = `GO HIGHER`;
+  } else {
+    spanBoxElement.innerText = userInput.value;
+    messageBoxElement.lastElementChild.innerHTML = `GO LOWER`;
+  }
+
+  if (userInput.value > 100 || userInput.value <= 0) {
+    spanBoxElement.innerText = userInput.value;
+    messageBoxElement.lastElementChild.innerHTML = `Number must be between 1 and 100`;
+  }
+});
+
+playAgainBtn.addEventListener("click", () => {
+  window.location.reload();
+});
